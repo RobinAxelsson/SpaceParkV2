b5b1c31b44912becfaf284166a6aed4025f7338e
Albin <almalbin98@gmail.com>
2021-05-04 14:43:46 +0200

    Added authorization to register model and also token data model

---
+++ Source/SpacePark-API/Authentication/ParkingModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SpacePark_API.Authentication
+{
+    public class ParkingModelId
+    {
+        [Required(ErrorMessage = "Minutes is required")]
+        public double Minutes { get; set; }
+        [Required(ErrorMessage = "Spaceport-name is required")]
+        public int SpacePortId { get; set; }
+    }  
+    public class ParkingModelString
+    {
+        [Required(ErrorMessage = "Minutes is required")]
+        public double Minutes { get; set; }
+        [Required(ErrorMessage = "Spaceport-name is required")]
+        public string SpacePortName { get; set; }
+    }
+}
---
+++ Source/SpacePark-API/Models/UserToken.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SpacePark_API.Models
+{
+    public record UserToken
+    {
+        public Account Account { get; set; }
+        public string Token { get; set; }
+        public DateTime ExpiryDate { get; set; }
+    }
+}
