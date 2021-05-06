34ef9a19319525142a8a8619b7048e150abf136f
Albin <almalbin98@gmail.com>
2021-05-06 10:16:25 +0200

    Updated models

--- Source/SpacePark-API/Authentication/AdminModels.cs
+++ Source/SpacePark-API/Authentication/AdminModels.cs
@@ -8,40 +8,25 @@ namespace SpacePark_API.Authentication
     }
     public class ChangeSpacePortAvailabilityId
     {
-        public int SpacePortId { get; set; }
+        public string NameOrId { get; set; }
         public bool Enabled { get; set; }
     }
-    public class ChangeSpacePortAvailabilityString
-    {
-        public string Name { get; set; }
-        public bool Enabled { get; set; }
-    }
+  
     public class RemoveSpacePortModelId
     {
-        public int SpacePortId { get; set; }
+        public string NameOrId { get; set; }
     }
-    public class RemoveSpacePortModelString
-    {
-        public string Name { get; set; }
-    }
+
     public class UpdateSpacePortPriceId
     {
-        public int SpacePortId { get; set; }
+        public string NameOrId { get; set; }
         public double SpacePortMultiplier { get; set; }
     }
-    public class UpdateSpacePortPriceString
-    {
-        public string Name { get; set; }
-        public double SpacePortMultiplier { get; set; }
-    }
+   
     public class UpdateSpacePortNameId
     {
-        public int SpacePortId { get; set; }
+        public string NameOrId { get; set; }
         public string NewSpacePortName { get; set; }
     }
-    public class UpdateSpacePortNameString
-    {
-        public string OldSpacePortName { get; set; }
-        public string NewSpacePortName { get; set; }
-    }
+   
 }
--- Source/SpacePark-API/Controllers/ParkingController.cs
+++ Source/SpacePark-API/Controllers/ParkingController.cs
@@ -63,50 +63,6 @@ namespace SpacePark_API.Controllers
             return spacePort.CalculatePrice(spaceShip, minutes);
         }
 
-        #region Overloads
-        [Authorize]
-        [HttpPost]
-        //Overload for filtering SpacePorts based on name instead of ID
-        [Route("api/[controller]/Park")]
-        public IActionResult Post([FromBody] ParkingModelString model)
-        {
-            var spacePort = _repository.SpacePorts.Single(sp => sp.Name == model.SpacePortName);
-            var token = Request.Headers.FirstOrDefault(p => p.Key == "Authorization").Value.FirstOrDefault()?.Replace("Bearer ", "");
-            var account = _repository.UserTokens.Where(a => a.Token == token)
-                .Include(a => a.Account).Include(p => p.Account.Person).Include(ss => ss.Account.SpaceShip).Single()
-                .Account;
-            if (account == null || spacePort == null)
-                return NotFound();
-            var price = spacePort.CalculatePrice(account.SpaceShip, model.Minutes);
-            var receipt = new Receipt
-            {
-                Price = price,
-                StartTime = DateTime.Now.ToString("d"),
-                EndTime = DateTime.Now.AddMinutes(model.Minutes).ToString("d"),
-                SpacePort = spacePort,
-                Account = account
-            };
-            _repository.Add(receipt);
-            _repository.SaveChanges();
-
-            return Ok(new { 
-                StartTime = receipt.StartTime,
-                EndTime = receipt.EndTime,
-                Price = receipt.Price,
-                SpacePort = receipt.SpacePort.Name,
-                PurchasedBy = account.Person.Name
-            });
-        }
-        
-        [HttpGet]
-        [Route("api/[controller]/Price")]
-        //Overload for filtering SpacePorts based on name instead of ID
-        public decimal Get(string spacePortName, string spaceShipModel, double minutes)
-        {
-            var spacePort = _repository.SpacePorts.Single(sp => sp.Name == spacePortName);
-            var spaceShip = APICollector.ParseShipAsync(spaceShipModel);
-            return spacePort.CalculatePrice(spaceShip, minutes);
-        }
-        #endregion
+      
     }
 }
