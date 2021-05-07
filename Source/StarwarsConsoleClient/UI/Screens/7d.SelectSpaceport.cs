abd7961ea4f4b835d1386038d8b40b91cc3c5897
RobinAxelsson <axelsson.robin@outlook.com>
2021-05-07 22:04:07 +0200

    added select spaceport screen

---
+++ Source/StarwarsConsoleClient/UI/Screens/7d.SelectSpaceport.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using static StarwarsConsoleClient.Main.Program;
+using StarwarsConsoleClient.Main;
+using System.Threading.Tasks;
+
+namespace StarwarsConsoleClient.UI.Screens
+{
+    public static partial class Screen
+    {
+        public static async Task<Option> SelectSpacePort()
+        {
+            ConsoleWriter.ClearScreen();
+            var lines = File.ReadAllLines(@"/UI/maps/7d.SelectSpaceport.cs");
+            var drawables = TextEditor.Add.DrawablesAt(lines, 0);
+            var nextLine = drawables.Max(x => x.CoordinateY);
+            var ports = await Client.GetSpacePortsAsync();
+            var portLines = ports.Select(x => "$ " + x.name).ToArray();
+            drawables.AddRange(TextEditor.Add.DrawablesAt(portLines, nextLine + 3));
+            TextEditor.Center.ToScreen(drawables, Console.WindowWidth, Console.WindowHeight);
+
+            var selectionList = new SelectionList<SpacePort>(ForegroundColor, '$');
+            selectionList.GetCharPositions(drawables);
+            selectionList.AddSelections(ports.ToArray());
+            ConsoleWriter.TryAppend(drawables);
+            ConsoleWriter.Update();
+
+            var port = selectionList.GetSelection();
+            UserData.SelectedSpacePort = port;
+            return Option.Account;
+        }
+    }
+}
